75aafb5b1a61c4acc4210acecbc0e21de775dbaa
Стас Клюхин <stanislavak314@gmail.com>
2018-12-03 22:14:15 +0600

    load and display title of upcoming movies

--- NetworkLayers/NetworkLayers/Controllers/Movies/MoviesViewController.swift
+++ NetworkLayers/NetworkLayers/Controllers/Movies/MoviesViewController.swift
@@ -14,8 +14,15 @@ class MoviesViewController: UIViewController {
     
     private var movies: [Movie]?
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadMovies()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        registerCells()
     }
     
     private func setup() {
@@ -23,9 +30,13 @@ class MoviesViewController: UIViewController {
         tableView.delegate = self
     }
     
+    private func registerCells() {
+        tableView.register(UINib(nibName: MovieTableViewCell.nibName, bundle: nil), forCellReuseIdentifier: MovieTableViewCell.identifier)
+    }
+    
     private func loadMovies() {
         ActivityLoader.addLoader(to: view)
-        MoviesService.shared.getMovies(byPage: 0) { [weak self] (movies, error) in
+        MoviesService.shared.getMovies(byPage: 1) { [weak self] (movies, error) in
             guard let `self` = self else { return }
             DispatchQueue.main.async {
                 if let error = error {
@@ -48,10 +59,16 @@ extension MoviesViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        return dequeueMovieTableViewCell(for: indexPath)
     }
     
-    
+    private func dequeueMovieTableViewCell(for indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifier) as? MovieTableViewCell else { fatalError() }
+        if let movies = movies {
+            cell.movie = movies[indexPath.row]
+        }
+        return cell
+    }
 }
 
 extension MoviesViewController: UITableViewDelegate {
