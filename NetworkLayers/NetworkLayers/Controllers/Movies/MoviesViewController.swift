b214b20fc3eb8cc1a23b65c1d5f9047667501538
Стас Клюхин <stanislavak314@gmail.com>
2018-12-03 20:52:37 +0600

    add load movies logic to movies view controller

--- NetworkLayers/NetworkLayers/Controllers/Movies/MoviesViewController.swift
+++ NetworkLayers/NetworkLayers/Controllers/Movies/MoviesViewController.swift
@@ -12,21 +12,39 @@ class MoviesViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
     
+    private var movies: [Movie]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     private func setup() {
         tableView.dataSource = self
         tableView.delegate = self
     }
+    
+    private func loadMovies() {
+        ActivityLoader.addLoader(to: view)
+        MoviesService.shared.getMovies(byPage: 0) { [weak self] (movies, error) in
+            guard let `self` = self else { return }
+            DispatchQueue.main.async {
+                if let error = error {
+                    AlertUtils.showErrorAlert(withMessage: error.localizedDescription)
+                } else if let movies = movies {
+                    self.movies = movies
+                    self.tableView.reloadData()
+                }
+                ActivityLoader.removeLoader()
+            }
+
+        }
+    }
 
 }
 
 extension MoviesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return movies?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
